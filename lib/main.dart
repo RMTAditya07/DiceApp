79f0b8d07cd2c6d766a0018712b6611e3c1f6cd0
RMTAditya07 <21f1005024@ds.study.iitm.ac.in>
2024-10-05 14:13:07 +0530

    gradient colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255,0,55,34),
-        body: Center(
-          child: Text('Hello world'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [const Color.fromARGB(255, 26,2,80),Color.fromARGB(255,45,7,98)]),
+          ),
+          child: Center(
+            child: Text('Hello world'),
+          ),
         ),
       ),
     ),
